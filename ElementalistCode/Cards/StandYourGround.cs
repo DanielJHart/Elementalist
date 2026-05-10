90e4991f5293ba8d1f75e465da72d7c41deb6754
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-05-10 21:36:07 +0100

    Added Stand Your Ground

---
+++ ElementalistCode/Cards/StandYourGround.cs
@@ -0,0 +1,33 @@
+﻿using Elementalist.ElementalistCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace Elementalist.ElementalistCode.Cards;
+
+public class StandYourGround() : ElementalistCard(3, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<IntangiblePower>(1)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<WeakPower>(), HoverTipFactory.FromPower<IntangiblePower>()];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Ethereal, CardKeyword.Exhaust];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        if (play.Target == null || !play.Target.HasPower<WeakPower>())
+            return;
+
+        StandYourGround cardSource = this;
+        await CreatureCmd.TriggerAnim(cardSource.Owner.Creature, "Cast", cardSource.Owner.Character.CastAnimDelay);
+        IntangiblePower? intangiblePower = await PowerCmd.Apply<IntangiblePower>(cardSource.Owner.Creature, cardSource.DynamicVars["IntangiblePower"].BaseValue, cardSource.Owner.Creature, cardSource);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.EnergyCost.UpgradeBy(-1);
+    }
+}
