268a13a65751f72a1f88ac12c6b628945791582c
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-05-13 21:49:01 +0100

    Added the boulder

---
+++ ElementalistCode/Cards/TheBoulder.cs
@@ -0,0 +1,30 @@
+﻿using Elementalist.ElementalistCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Elementalist.ElementalistCode.Cards;
+
+public class TheBoulder() : ElementalistCard(3,
+    CardType.Attack, CardRarity.Common,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(25m, ValueProp.Move)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        if (play.Target == null)
+            return;
+        
+        await CreatureCmd.Damage(choiceContext, play.Target, this.DynamicVars.Damage, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Damage.UpgradeValueBy(7m);
+    }
+}
