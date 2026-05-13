7e7ab6b9baa8386dc6b1faba72926a4a1b98b831
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-05-13 16:48:22 +0100

    Added Bolster card

---
+++ ElementalistCode/Cards/Bolster.cs
@@ -0,0 +1,35 @@
+﻿using Elementalist.ElementalistCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Elementalist.ElementalistCode.Cards;
+
+public class Bolster() : ElementalistCard(1, CardType.Skill, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(2, ValueProp.Move)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<WeakPower>()];
+    
+    public override bool GainsBlock => true;
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        if (play.Target == null)
+            return;
+
+        int weakCount = play.Target.GetPowerAmount<WeakPower>();
+        await CreatureCmd.GainBlock(this.Owner.Creature, this.DynamicVars.Block.BaseValue * weakCount, ValueProp.Move, play);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Block.UpgradeValueBy(1);
+    }
+}
