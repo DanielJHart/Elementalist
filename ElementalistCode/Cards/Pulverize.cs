d783ed7640d719c463d5453d1a9ed67372393169
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-05-17 10:03:27 +0100

    Added Pulverize

---
+++ ElementalistCode/Cards/Pulverize.cs
@@ -0,0 +1,34 @@
+﻿using Elementalist.ElementalistCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Elementalist.ElementalistCode.Cards;
+
+public class Pulverize() : ElementalistCard(2,
+    CardType.Attack, CardRarity.Uncommon,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(15, ValueProp.Unblockable)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Block)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        if (play.Target == null)
+            return;
+        
+        VfxCmd.PlayOnCreatureCenter(play.Target, "vfx/vfx_attack_blunt");
+        await CreatureCmd.Damage(choiceContext, play.Target, this.DynamicVars.Damage.BaseValue, ValueProp.Unblockable,  this.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Damage.UpgradeValueBy(5);
+    }
+}
