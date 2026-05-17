a02657156340fec493517877be951717e2fece20
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-05-17 11:20:33 +0100

    Added Hot Rocks

---
+++ ElementalistCode/Cards/HotRocks.cs
@@ -0,0 +1,34 @@
+﻿using Elementalist.ElementalistCode.Cards;
+using Elementalist.ElementalistCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Elementalist.ElementalistCode.Cards;
+
+public class HotRocks() : ElementalistCard(2,
+    CardType.Power, CardRarity.Uncommon,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(2, ValueProp.Move)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Block), HoverTipFactory.FromPower<BurnPower>()];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        if (play.Target == null)
+            return;
+        
+        await PowerCmd.Apply<HotRocksPower>(play.Target, this.DynamicVars.Block.BaseValue, this.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Block.UpgradeValueBy(1);
+    }
+}
---
+++ ElementalistCode/Powers/HotRocksPower.cs
@@ -0,0 +1,27 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Elementalist.ElementalistCode.Powers;
+
+public class HotRocksPower : ElementalistPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override Task AfterPowerAmountChanged(PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        if (power.Owner == this.Owner 
+            && power is BurnPower 
+            && amount > 0 
+            && this.Applier != null 
+            && applier == this.Applier)
+        {
+            CreatureCmd.GainBlock(this.Applier, Amount, ValueProp.Move, null);
+        }
+        
+        return base.AfterPowerAmountChanged(power, amount, applier, cardSource);
+    }
+}
