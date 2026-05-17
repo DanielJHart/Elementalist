196ce952c367a23a5b0152daf5beda951961ac73
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-05-17 10:24:40 +0100

    Added Fountain of Youth

---
+++ ElementalistCode/Cards/FountainOfYouth.cs
@@ -0,0 +1,27 @@
+﻿using Elementalist.ElementalistCode.Cards;
+using Elementalist.ElementalistCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Elementalist.ElementalistCode.Cards;
+
+public class FountainOfYouth() : ElementalistCard(3,
+    CardType.Power, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PowerCmd.Apply<FountainOfYouthPower>(this.Owner.Creature, 1, this.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Energy.UpgradeValueBy(1);
+    }
+}
---
+++ ElementalistCode/Powers/FountainOfYouthPower.cs
@@ -0,0 +1,15 @@
+﻿using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+
+namespace Elementalist.ElementalistCode.Powers;
+
+public class FountainOfYouthPower : ElementalistPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.None;
+    
+    public override Decimal ModifyMaxEnergy(Player player, Decimal amount)
+    {
+        return player != this.Owner.Player ? amount : amount + (Decimal) this.Amount;
+    }
+}
